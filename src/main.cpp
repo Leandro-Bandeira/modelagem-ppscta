3a2a2cc7c4c2edef872dfb15f4c4f9e08917244a
Leandro Lucas <leoViolinist22@gmail.com>
2022-08-17 20:25:25 -0300

    adicionando makefile e começando codigo

--- src/main.cpp
+++ src/main.cpp
@@ -4,55 +4,148 @@
 #include <fstream>
 #include <sstream>
 #include <stdlib.h>
+#include "ilcplex/ilocplex.h"
 
-void matrizBeneficios(const std::string& caminho) {
-	
-	std::fstream* ptrArquivo = new std::fstream(caminho, std::ios::in);
-	
-	int colunas, linhas;
 
-	std::string linha;
+
+
+
+
+
+
+
+int **matrizBeneficios(const std::string& caminho, int& quantiaOrientadores, int& quantiaTrabalhos) {
+	
+	std::fstream* ptrArquivo = new std::fstream(caminho, std::ios::in); // Abre o arquivo para entrada de dados
+	
+	std::string linha; // Linha lida no arquivo
 	
 	std::vector < int > valoresLinha; // Valores que vão estar na linha
-	
-	while(1) {
 
-		std::getline(*ptrArquivo, linha);
-		if(ptrArquivo->eof()) {
 
-			break;
-		}
+
+	int repeticoes = 0;
+
+
+
+	std::getline(*ptrArquivo, linha);
+
+	std::stringstream dadosLinha(linha); // Cria uma stream de string da linha
+	std::string palavra; // Variável onde será armazenada temporariamente a palavra
+
+	while(std::getline(dadosLinha, palavra, ' ')) {
 			
-
-		std::stringstream dadosLinha(linha);
-		std::string palavra;
-
-		while(std::getline(dadosLinha, palavra, ' ')) {
-			
-			valoresLinha.push_back(atoi(palavra.c_str()));
-
-
-		}
-
-		for(int i  = 0; i < valoresLinha.size(); i++) {
-
-			std::cout << valoresLinha[i] << " ";
-		}
-		std::cout << std::endl;
-		valoresLinha.clear();
-		break;
-
+		valoresLinha.push_back(atoi(palavra.c_str())); // Converte para inteiro e colocar no vector valores da linha
 	}
+		
+			
+	quantiaOrientadores = valoresLinha[0];
+	quantiaTrabalhos = valoresLinha[1];
+
+		
+		
+
+	
+	valoresLinha.clear(); // Limpa o vector
+	linha.clear();
+
+	/* Criação da matriz de beneficios	*/
+	int** matriz = new int*[quantiaOrientadores];
+
+	for(int i = 0; i < quantiaOrientadores; i++) {
+
+		matriz[i] = new int[quantiaTrabalhos];
+	}
+	
+
+	int indiceLinha = 0;
+	while(1) {
+		
+
+		std::getline(*ptrArquivo, linha); // Ler a linha
+
+		if(ptrArquivo->eof())
+			break;
+
+
+		std::stringstream dados(linha); // Crima uma stream de string
+		
+		// Separa os dados da string de stream pelo espaço e então armazena no vector de valores
+		while(std::getline(dados, palavra, ' ')) {
+
+			valoresLinha.push_back(atoi(palavra.c_str()));
+		}
+		
+		// Percorre em relação a coluna, mantendo o indice da linha fixo
+		for(int j = 0; j < valoresLinha.size(); j++) {
+			
+			matriz[indiceLinha][j] = valoresLinha[j];
+
+		}
+
+		/* Limpeza de todos os dados	*/
+		valoresLinha.clear();
+		linha.clear();
+		indiceLinha++;
+
+	} 
+		 
 
 	ptrArquivo->close();
 	delete ptrArquivo;
+	
 
+	return matriz;
 
 
 }
 
 
 
+
+void resolveModelo(int** beneficios, int quantiaOrientadores, int quantiaTrabalhos) {
+
+	IloEnv env; // Cria o ambiente de modelagem
+
+	/* Criação do modelo, responsável por função objetivo e restrições	*/
+	/* Precisa de um ambiente de modelagem para ser iniciado	*/
+	IloModel Model(env);
+
+
+	/* Criação da variavel de decisão	*/
+	// Devemos enviar os parametros de ambiente  e quantia de linha inicial
+
+	/* Utilização do template de classe IloArray para criar um	array de duas dimensões*/
+	/* Do tipo IloNumVarArray, chamado x*/
+	IloArray < IloNumVarArray > x(env, quantiaOrientadores);
+
+	/* Acessamos cada valor dessa linha inicial, e iniciamos um array com o ambiente, tamanho, indice incial e assim	*/
+	for(int i = 0; i < quantiaOrientadores; i++) {
+		
+		// Criamos um array com numero de colunas igual a quantia de trabalho
+		// Limite inferior 0 e limite superior 1, todos como inteiro
+		x[i] = IloNumVarArray(env, quantiaTrabalhos, 0, 1, ILOINT);
+	}
+
+
+	/* Criação da função objetivo	*/
+	IloExpr exp0(env); // Inicializa uma expressão
+
+	
+	for(int i = 0; i < quantiaOrientadores; i++) {
+
+
+		for(int j = 0; j < quantiaTrabalhos; j++) {
+			
+			exp0 += beneficios[i][j] * x[i][j];
+
+		}
+	}
+
+	Model.add(IloMaximize(env, exp0)); // Adiciona ao modelo para maximizar a função
+	
+
+}
 
 
 
@@ -70,8 +163,17 @@ int main(int argc, char** argv) {
 	int quantiaTrabalhos = 0;
 
 	
-	matrizBeneficios(argv[1]);
+	int** beneficios = matrizBeneficios(argv[1], quantiaOrientadores, quantiaTrabalhos);
+	
+	resolveModelo(beneficios, quantiaOrientadores, quantiaTrabalhos);
 
+	
+	for(int i = 0; i < quantiaOrientadores; i++) {
+
+		delete beneficios[i];
+	}
+
+	delete beneficios;
 
 	return 0;
 }
