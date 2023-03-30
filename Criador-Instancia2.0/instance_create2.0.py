1706bd90ed61a8a3d46b41b6b96b598d31d92611
Leandro <leoviolinist22@gmail.com>
2023-03-30 17:47:18 -0300

    ajeitando dados e instancia2.0

--- Criador-Instancia2.0/instance_create2.0.py
+++ Criador-Instancia2.0/instance_create2.0.py
@@ -7,15 +7,28 @@ def open_similarity(path_similarity):
 
     with open(path_similarity) as file:
         data = json.load(file)
+    file.close()
+
     return data
 
+
+def open_projects(path_projects):
+    data = dict()
+    with open(path_projects) as file:
+        data = json.load(file)
+    file.close()
+
+    return data
 
 
 def main():
     path_similarity = "../SimilaridadesOrientadores/similarityOrientadores14Alin.json"
+    path_projetcs = "projetos2014.json"
 
-
+    # Abrindo arquivos de json e armazenandos seus dicionários
+    projetcs_data = open_projects(path_projetcs)
     similarity_dic = open_similarity(path_similarity) 
+
 
 
 
