5a621067424d524752c77412bb222dee84332c63
Leandro Lucas <leoViolinist22@gmail.com>
2023-04-03 18:00:42 -0300

    ajeitando arquivos

--- Criador-Instancia2.0/instance_create.py
+++ Criador-Instancia2.0/instance_create.py
@@ -5,111 +5,104 @@ import json
 
 def open_similarity(path):
 
-    arq = open(path, 'r')
-    lines = arq.readlines()
-    arq.close()
+    similarity_data = list()
 
-    similarity_dic = list() 
-    for i in range(len(lines) - 1):
-        data = dict()
-        data["indice"] = lines[i].rstrip() 
-        data["similarity"] = lines[i + 1].rstrip().split()
-        similarity_dic.append(data)
-
-    return similarity_dic
+    with open(path) as file:
+        similarity_data = json.load(file)
+    file.close()
+    return similarity_data
 
 # retorna uma lista contendo todos os nomes dos orientadore
 
 
-def open_orientadores(path, len_projects, orientadores_list):
+def open_orientadores(path):
 
-    orientadores_data = list()
-    orientadores = list()
+    projetos_data = list()
     with open(path) as file:
-        orientadores_data = json.load(file)
-        # Armazena todos os dados do orientadores
-        orientadores_list.append(orientadores_data)
-        len_projects.append(len(orientadores_data))
-        for data in orientadores_data:
-            orientadores.append(data["Orientador:"])
-
-    orientadores = list(set(orientadores))
+        projetos_data = json.load(file)
+        
     file.close()
-    return orientadores
+    return projetos_data
 
 
 
 def main():
-    path_similarity = "../SimilaridadesOrientadores/similarityOrientadores14Alin.txt"
-    path_orientadores = "resumoOrientadores/resumoOrientadores14Alin.json"
+    path_similarity = "../DadosTrabalhos/similarityOrientadores14Alin.json"
+    path_orientadores = "../DadosTrabalhos/projetos2014.json"
     path_instance = "instance14Alin.txt"
-    orientadores_list = list()  # Contém todos os dados dos orientadores
-    similarity_data = open_similarity(path_similarity)
-    len_projects = list()
+    
+    similarity_data = open_similarity(path_similarity) # Contém os dados de similaridade
+    
+    projetos_data = open_orientadores(path_orientadores)
+    
+    orientadores_data = list() # Contém todos os orientadores exclusivamente
 
-    orientadores_data = open_orientadores( path_orientadores, len_projects, orientadores_list)
-    # Contém o dicionario dos orientadores
-    orientadores_dic = orientadores_list[0] #Todo o arquivo json de orientadores
+    for projeto in projetos_data:
+        if projeto["Orientador:"] not in orientadores_data:
+            orientadores_data.append(projeto["Orientador:"])
+    
 
+    # Abre o arquivo para armazenar a quantia de orientadores x projetos
     arq = open(path_instance, 'w')
 
-    line = str(len(orientadores_data)) + ' ' + str(len_projects[0]) + '\n'
+    line = str(len(orientadores_data)) + ' ' + str(len(projetos_data)) + '\n'
     arq.write(line)
 
     # Criação da instancia para inviablizar pesos para trabalhos do mesmo orientador, caso forem o mesmo, temos -1 como peso
     # Além disso, precisamos criar uma matriz orientadores X Trabalhos, contendo suas similaridades, vamos analisar qual a melhor matriz similaridade de determinado orientador
-    # E usá-la como o peso para o algoritmo
+    # E usá-la como o peso para o algoritmo, muito importante perceber que os indices dos dois ARQUIVOS estão equiparados, ou seja a similaridade do projeto 0 equivale ao projeto 0.
 
     orientadores_visitados = list()
     i = 0
     
-    for similarity in similarity_data:
-        print(similarity)
-        a = input()
     
-    # Orientadores_dic representa todo o arquivo json, então vamos andar por todas as "instancias" de orientadores, que seriam todos os seus trabalhos e adicionar a uma lista daquele orientador ja visitado
-    # Após isso, percorremos a matriz quadrada de similaridade, verificando qual linha representa ao orientador adicionado a lista, achado a sua linha, calculamos a media aritmetica daquela linha de similaridade
-    # Verifica se é maior do que a antiga e atualiza. Fazemos isso com todas as "instancias" do mesmo orientador para achar a sua melhor similaridade, após isso com a linha de melhor similaridade encontrada
-    # Percorremos novamente a matriz, verificando se o trabalho da coluna é do mesmo orientador, para indicar que é viável alocar ele naquele trabalho e então adicionamos no arquivo de instancia
-    # o loop continua até que todos os orientadores sejam varridos. Criando assim, uma matriz orientador x similaridade
-    # Vamos normalizar a similaridade
-    # Valores entre 0 e 5% recebem 1 como afindade, 5% e 40% recebem 10 e o restante 100
-    # Precisamos indicar o indice do orientador
-    while (i < len(orientadores_dic)):
+    
+    # A ideia agora é achar a partir de similarity_data o trabalho que possui maiores similaridades
+    # Achado o trabalho do orientador com maiores similaridades, vamos utilizar um padrão dependendo da similaridade
+    # e indicar que aquelas similaridades são daquele indice que representa o trabalho dele
+    while (i < len(projetos_data)):
         
-        if orientadores_dic[i]['Orientador:'] not in orientadores_visitados:
-            orientadores_visitados.append(orientadores_dic[i]['Orientador:'])
+        if projetos_data[i]['Orientador:'] not in orientadores_visitados:
+            orientadores_visitados.append(projetos_data[i]['Orientador:'])
             media = 0
-            indice_maior_media = 0
+            # Se tivermos situação em que ele só tiver um projeto, então ele não entra no segundo for, como os indices estão iguais, da similaridade e do projeto, a similaridade de indice 0 representa as similaridades do mesmo projeto 0
+            indice_maior_media = i 
             maior_media = -1
 
             #Acha a melhor linha de similaridade do orientador anterior
-            for j in range(len(similarity_data)):
-
-                if orientadores_dic[j]["Orientador:"] == orientadores_dic[i]["Orientador:"]:
-                        
-                    for k in range(len(similarity_data[j])):
-                        media += float(similarity_data[j][k])
-                    media /= len(similarity_data[j])
+            for similarity in similarity_data:
+                indice = similarity["indice"]
+                data_similarity = similarity["similaridade"].rstrip().lstrip().split()
+                
+                # Se encontrarmos o mesmo orientador, daquele fixdo em i na minha similaridade
+                if projetos_data[indice]["Orientador:"] == projetos_data[i]["Orientador:"]:
+                    
+                    for value in data_similarity:
+                        media += float(value)
+                    media /= len(data_similarity)
 
                     if media > maior_media:
                         maior_media = media
-                        indice_maior_media = j
-            # Armazena a melhor linha do orientador no arquivo de instancia
-            for j in range(len(similarity_data[indice_maior_media])):
+                        indice_maior_media = indice
+            
+
+            # Pegamos entãoo a similaridade de maior indice daquele orientador
+            avg_similarity = similarity_data[indice_maior_media]["similaridade"].rstrip().lstrip().split()
+            for j, similarity in enumerate(avg_similarity):
                 # Se o orientador da linha for o mesmo da coluna, então -1 para indicar que o trabalho é inviável
-                # Cso ele não for inviável, vamos normalizar as similares
+                # Cso ele não for inviável, vamos normalizar as similaridades
                 similarity = 0
-                if orientadores_dic[indice_maior_media]["Orientador:"] == orientadores_dic[j]["Orientador:"]:
+                if projetos_data[indice_maior_media]["Orientador:"] == projetos_data[j]["Orientador:"]:
                     similarity = "-1"
                 else:
-                    if float(similarity_data[indice_maior_media][j]) <= 0.05:
-                        similarity = "1"
-                    elif float(similarity_data[indice_maior_media][j]) <= 0.4:
+                    if float(similarity) > 0.3:
+                        similarity = "100"
+                    elif float(similarity) >= 0.1 and float(similarity) <= 0.3:
                         similarity = "10"
                     else:
-                        similarity = "100"
-                if j == len(similarity_data[indice_maior_media]) - 1:
+                        similarity = "1"
+                
+                if j == len(avg_similarity) - 1:
 
                     line = similarity + '\n'
                 else:
